ec50ddce215c1f9551ad7995eba03ff41c565146
Tota <71022738+Tota6@users.noreply.github.com>
2023-02-16 23:39:13 +0200

    creating th login class and make it flexible on all device

---
+++ lib/features/login/presentation/screens/login_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_starter/core/utils/app_colors.dart';
+import 'package:flutter_starter/core/utils/app_text_style.dart';
+
+import '../../../../core/validation/validation_types.dart';
+import '../../../../core/widgets/buttons/button_form_widget.dart';
+import '../../../../core/widgets/forms/text_field_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        padding: const EdgeInsets.only(top: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Image.asset(
+                'assets/images/2x/Logo.png',
+                scale: 1.5,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0, bottom: 60),
+              child: Text(
+                'What we try to do is to reach the \ncriminal together and get the best \n\t\t\t\t\tresult in the shortest time.',
+                style: AppTextStyle.loginDetailsText,
+              ),
+            ),
+            Container(
+              height: screenHeight / 2,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(70),
+                    topLeft: Radius.circular(70),
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Color.fromRGBO(48, 48, 48, 1),
+                        offset: Offset(0, -2),
+                        blurRadius: 8,
+                        blurStyle: BlurStyle.normal,
+                        spreadRadius: 2),
+                  ],
+                  color: AppColors.background),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Please Login with your generated \n\t\t\t\t\taccount that you received',
+                      style: AppTextStyle.loginText,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 40.0, bottom: 18, left: 15, right: 15),
+                      child: TextFieldWidget(
+                        label: 'Email',
+                        labelStyle: AppTextStyle.loginDetailsText,
+                        errorStyle: TextStyle(color: AppColors.error),
+                        borderWidth: 1,
+                        borderColor: AppColors.grey,
+                        errorBorderColor: AppColors.error,
+                        secureText: true,
+                        validateType: ValidationTypes.signinPassword,
+                        keyboardType: TextInputType.emailAddress,
+                        onSave: (_) {},
+                        textAlign: TextAlign.start,
+                        contentPadding: const EdgeInsets.only(
+                          top: 15,
+                          left: 30,
+                        ),
+                        style: AppTextStyle.loginDetailsText,
+                        cursorColor: AppColors.secondaryColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, right: 15),
+                      child: TextFieldWidget(
+                        label: 'Password',
+                        labelStyle: AppTextStyle.loginDetailsText,
+                        errorStyle: TextStyle(color: AppColors.error),
+                        borderWidth: 1,
+                        borderColor: AppColors.grey,
+                        errorBorderColor: AppColors.error,
+                        secureText: true,
+                        validateType: ValidationTypes.signinPassword,
+                        keyboardType: TextInputType.visiblePassword,
+                        onSave: (_) {},
+                        textAlign: TextAlign.start,
+                        contentPadding: const EdgeInsets.only(
+                          top: 15,
+                          left: 30,
+                        ),
+                        style: AppTextStyle.loginDetailsText,
+                        cursorColor: AppColors.secondaryColor,
+                      ),
+                    ), // Padding(
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: ButtonFormWidget(
+                        onPress: () {},
+                        child: const Text(
+                          'Login',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
