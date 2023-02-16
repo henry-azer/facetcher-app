454e5ec0965e54caebe0a60e0e673cdbf8a14df9
Tota <71022738+Tota6@users.noreply.github.com>
2023-02-16 23:29:40 +0200

    adding the app colors needed

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -7,20 +7,23 @@ class AppColors {
   static Color hint = HexColor('#FFFFFF');
 
   static Color error = HexColor('#900C3F');
-  static Color background = HexColor('#000020');
+  static Color background = HexColor('#3D4149');
 
-  static Color fontPrimary = HexColor('#FFFFFF');
-  static Color fontSecondary = HexColor('#E8C39E');
+  static Color fontPrimary = HexColor('#EAEAEA');
+  static Color fontSecondary = HexColor('#B3B4B');
 
-  static Color border = HexColor('#2F2C79');
+  static Color primaryColor = HexColor('#B3B4B4');
+  static Color secondaryColor = HexColor('#07ABB3');
 
-  static Color button = HexColor('#2F2C79');
+  static Color border = HexColor('#B3B4B4');
+
+  static Color button = HexColor('#07ABB3');
   static Color snackbar = HexColor('#2F2C79');
   static Color navigator = HexColor('#000020');
 
   static Color black = HexColor('#000000');
-  static Color white = HexColor('#FFFFFF');
-  static Color grey = HexColor('#808080');
+  static Color white = HexColor('#EAEAEA');
+  static Color grey = HexColor('#B3B4B4');
   static Color gold = HexColor('#E8C39E');
 
   static Color underlinePrimary = HexColor('#2F2C79');
