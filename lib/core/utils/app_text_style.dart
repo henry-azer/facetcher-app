076250cb28dc8a969c6b5b12ffdfd5a0ffd9a927
Tota <71022738+Tota6@users.noreply.github.com>
2023-02-26 14:11:17 +0200

    adding the needed font style to text style widget

--- lib/core/utils/app_text_style.dart
+++ lib/core/utils/app_text_style.dart
@@ -25,4 +25,71 @@ class AppTextStyle with Diagnosticable {
     color: AppColors.white,
     fontFamily: AppStrings.fontFamilyComfortaa,
   );
+  static TextStyle? profileText = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w700,
+    color: AppColors.white,
+    fontFamily: AppStrings.fontFamilyComfortaa,
+  );
+  static TextStyle? profileNiceText = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.white,
+    fontFamily: AppStrings.fontFamilyComfortaa,
+  );
+
+  static TextStyle? profileFieldText = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+    fontFamily: AppStrings.fontFamilyComfortaa,
+  );
+  static TextStyle? loginDetailsText = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: AppColors.grey,
+      fontFamily: AppStrings.fontFamilyBarlow,
+      height: 1.2);
+
+  static TextStyle? drawingDetailsFieldTitle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: AppColors.grey,
+    fontFamily: AppStrings.fontFamilyBarlow,
+  );
+  static TextStyle? screensTitle = TextStyle(
+      fontSize: 35,
+      fontWeight: FontWeight.w700,
+      color: AppColors.white,
+      fontFamily: AppStrings.fontFamilyComfortaa,
+      wordSpacing: 5);
+
+  static TextStyle? screensTitleDetails = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+    fontFamily: AppStrings.fontFamilyComfortaa,
+  );
+
+  static TextStyle? drawingDetailsField = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.grey,
+    fontFamily: AppStrings.fontFamilyBarlow,
+  );
+
+  static TextStyle? navigatorItem = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    color: AppColors.navigatorFont,
+    fontFamily: AppStrings.fontFamilyComfortaa,
+  );
+
+  static TextStyle? loginText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+    fontFamily: AppStrings.fontFamilyComfortaa,
+    height: 1,
+  );
 }
