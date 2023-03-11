88360de27ed90ab61a9ed90a52aec2eea3b9fc7c
Tota <71022738+Tota6@users.noreply.github.com>
2023-03-11 17:46:58 +0200

    merging the branch

--- lib/core/widgets/app_bar_widget.dart
+++ lib/core/widgets/app_bar_widget.dart
@@ -15,7 +15,7 @@ class _AppBarWidgetState extends State<AppBarWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 40, right: 40, top: 65, bottom: 20),
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 45, bottom: 10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -23,16 +23,11 @@ class _AppBarWidgetState extends State<AppBarWidget> {
             icon: Icon(Icons.arrow_back, color: AppColors.white),
             onPressed: () => Navigator.pop(context),
           ),
-          GestureDetector(
-            onTap: () {
-              // TODO : change appHome route to whatever you want
-              Navigator.pushReplacementNamed(context, Routes.appHome);
-            },
-            child: Image.asset(
-              ImgAssets.user,
-              width: 35,
-              height: 50,
-            ),
+
+          IconButton(
+            icon: Icon(Icons.menu, color: AppColors.white),
+            onPressed: () => Navigator.pushReplacementNamed(context, Routes
+                .appHome),
           ),
         ],
       ),
--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,11 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_starter/config/themes/app_theme.dart';
-import 'package:flutter_starter/core/utils/app_colors.dart';
-
 import '../../../../config/routes/app_routes.dart';
-import '../../../../core/utils/app_text_style.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -44,10 +39,11 @@ class _SplashScreenState extends State<SplashScreen>
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Hero(
-                tag: 'Logo',
+              AnimatedSize(
+                duration: const Duration(seconds: 2),
+                curve: Curves.elasticInOut,
                 child: Image.asset('assets/images/2x/Logo.png'),
-              )
+              ),
             ],
           ),
         ),
