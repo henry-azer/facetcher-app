fd435e982c34eaad55eb247d80cacfc8c0f24f21
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-04-06 15:50:31 +0200

    edit navigator in user_history_screen.dart

--- lib/features/user-history/presentation/screens/user_history_screen.dart
+++ lib/features/user-history/presentation/screens/user_history_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_text_style.dart';
+import '../../../../core/widgets/app_bar_widget.dart';
+import '../../../../core/widgets/icons/animated_icon_button.dart';
 import '../../../../core/widgets/navigator/navigation_bar_wrapper.dart';
 
 class UserHistoryScreen extends StatefulWidget {
@@ -27,35 +29,29 @@ class _UserHistoryScreenState extends State<UserHistoryScreen> {
       path: ModalRoute.of(context)?.settings.name,
       child: Material(
         child: Scaffold(
-            appBar: AppBar(
-              centerTitle: false,
-              title: Padding(
-                padding: const EdgeInsets.only(top: 30.0, left: 30.0),
-                child: Text(
-                  "Hello Developer!",
-                  style: AppTextStyle.homeText,
-                ),
-              ),
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 25.0, right: 35.0),
-                  child: IconButton(
-                      onPressed: () => _handleToggleNavigationBar(),
-                      icon: const Icon(
-                        Icons.menu,
-                        size: 30,
-                      ),
-                      color: AppColors.white),
-                ),
-              ],
-            ),
             extendBody: true,
             resizeToAvoidBottomInset: true,
-            body: Center(
-              child: Text(
-                "History",
-                style: AppTextStyle.homeText,
-              ),
+            body: Column(
+              children: [
+                AppBarWidget(
+                  leftChild: IconButton(
+                    icon: Icon(Icons.arrow_back, color: AppColors.white),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  rightChild: AnimatedIconButton(
+                    icon: AnimatedIcons.menu_close,
+                    color: AppColors.fontPrimary,
+                    onPressed: () => _handleToggleNavigationBar(),
+                    durationMilliseconds: 500,
+                    size: 32.0,
+                    end: 1.0,
+                  ),
+                ),
+                Text(
+                  "History",
+                  style: AppTextStyle.homeText,
+                ),
+              ],
             )),
       ),
     );
