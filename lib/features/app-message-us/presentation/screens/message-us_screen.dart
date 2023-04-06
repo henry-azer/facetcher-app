572b57a7d44d3ea8e6e44c5c98f8b8ed5bd7e95d
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-04-06 13:52:34 +0200

    solve the error of keyboard overflow and add text in the top of screen

--- lib/features/app-message-us/presentation/screens/message-us_screen.dart
+++ lib/features/app-message-us/presentation/screens/message-us_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/utils/app_colors.dart';
+import '../../../../core/utils/app_text_style.dart';
 import '../../../../core/widgets/app_bar_widget.dart';
 import '../../../../core/widgets/icons/animated_icon_button.dart';
 import '../../../../core/widgets/navigator/navigation_bar_wrapper.dart';
+import '../widgets/message_us_form.dart';
 
 class MessageUs extends StatefulWidget {
   const MessageUs({Key? key}) : super(key: key);
@@ -20,6 +22,7 @@ class _MessageUsState extends State<MessageUs> {
       _toggleNavigationBar = true;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return NavigationBarWrapper(
@@ -29,23 +32,47 @@ class _MessageUsState extends State<MessageUs> {
         child: Scaffold(
           extendBody: true,
           resizeToAvoidBottomInset: true,
-          body: Column(
-            children: [
-              AppBarWidget(
-                leftChild: IconButton(
-                  icon: Icon(Icons.arrow_back, color: AppColors.white),
-                  onPressed: () => Navigator.pop(context),
+          body: SingleChildScrollView(
+            physics: const ScrollPhysics(),
+            child: Column(
+              children: [
+                AppBarWidget(
+                  leftChild: IconButton(
+                    icon: Icon(Icons.arrow_back, color: AppColors.white),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  rightChild: AnimatedIconButton(
+                    icon: AnimatedIcons.menu_close,
+                    color: AppColors.fontPrimary,
+                    onPressed: () => _handleToggleNavigationBar(),
+                    durationMilliseconds: 500,
+                    size: 32.0,
+                    end: 1.0,
+                  ),
                 ),
-                rightChild: AnimatedIconButton(
-                  icon: AnimatedIcons.menu_close,
-                  color: AppColors.fontPrimary,
-                  onPressed: () => _handleToggleNavigationBar(),
-                  durationMilliseconds: 500,
-                  size: 32.0,
-                  end: 1.0,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Get in Touch",
+                        style: AppTextStyle.drawingScreenTitle,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0, bottom: 30.0),
+                        child: Text(
+                          "Feel free to drop us a line below!",
+                          textAlign: TextAlign.center,
+                          style: AppTextStyle.drawingScreenTitleDetails,
+                        ),
+                      ),
+                      const MessageUsForm(),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
