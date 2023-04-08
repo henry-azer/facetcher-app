6724415bf6ec32a42ca8eaa4e0d583791cb301a1
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-04-09 00:15:38 +0200

    create message us repository implementation

---
+++ lib/data/repositories/message-us/message_us_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:facetcher/core/models/response_model.dart';
+import 'package:facetcher/data/datasources/message-us/message_us_remote_datasource.dart';
+import 'package:facetcher/data/entities/message-us/user-message-us-request.dart';
+
+import 'package:facetcher/core/error/exceptions.dart';
+import 'package:facetcher/data/models/message-us/message_us.dart';
+import '../../../core/network/network_info.dart';
+import '../../datasources/message-us/message_us_local_datasource.dart';
+import 'message_us_repository.dart';
+
+class MessageUsRepositoryImpl implements MessageUsRepository {
+  final NetworkInfo networkInfo;
+  final MessageUsLocalDataSourceImpl messageUsLocalDataSourceImpl;
+  final MessageUsRemoteDataSourceImpl messageUsRemoteDataSourceImpl;
+
+  MessageUsRepositoryImpl({
+    required this.networkInfo,
+    required this.messageUsLocalDataSourceImpl,
+    required this.messageUsRemoteDataSourceImpl,
+  });
+
+  @override
+  Future<Either<GenericException, ResponseModel<MessageUs>>>
+      createOrUpdateMessageSubmission(MessageUsRequest messageUsRequest) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final messageUs = await messageUsRemoteDataSourceImpl
+            .createOrUpdateMessage(messageUsRequest);
+        return Right(messageUs);
+      } on GenericException catch (exception) {
+        return Left(exception);
+      }
+    } else {
+      return const Left(CacheException());
+    }
+  }
+}
