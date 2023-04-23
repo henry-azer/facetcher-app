c222c6fa41ffa5b6013c2a1d7fbdc9773f7b0e76
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-04-23 11:37:58 +0200

    add three screens using custom widget

--- lib/features/app-terms/presentation/screen/app_password_screen.dart
+++ lib/features/app-terms/presentation/screen/app_password_screen.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 
 import '../widget/term_screen_widget.dart';
 
-class AppTermsSecuredData extends StatefulWidget {
-  const AppTermsSecuredData({Key? key}) : super(key: key);
+class AppPassword extends StatefulWidget {
+  const AppPassword({Key? key}) : super(key: key);
 
   @override
-  State<AppTermsSecuredData> createState() => _AppTermsSecuredDataState();
+  State<AppPassword> createState() => _AppPassword();
 }
 
-class _AppTermsSecuredDataState extends State<AppTermsSecuredData> {
+class _AppPassword extends State<AppPassword> {
   @override
   Widget build(BuildContext context) {
     return TermWidget(
-      title: "Secured Data",
+      title: "Password",
       description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       imageNetwork:
           "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
       onpress: () {
-        Navigator.of(context).pushNamed('/app-terms-trial-recorded');
+        Navigator.of(context).pushNamed('/app-terms-password');
       },
     );
   }
