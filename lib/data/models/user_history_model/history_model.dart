03ea62535ebf374594f3c17de499a038c92ba193
Tota <71022738+Tota6@users.noreply.github.com>
2023-04-29 21:03:26 +0300

    git the data from the endpoints

---
+++ lib/data/models/user_history_model/history_model.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+
+History historyFromJson(String str) => History.fromJson(json.decode(str));
+
+String historyToJson(History data) => json.encode(data.toJson());
+
+class History {
+  History({
+    required this.info,
+    required this.results,
+  });
+
+  Info info;
+  List<Result> results;
+
+  factory History.fromJson(Map<String, dynamic> json) => History(
+    info: Info.fromJson(json["info"]),
+    results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "info": info.toJson(),
+    "results": List<dynamic>.from(results.map((x) => x.toJson())),
+  };
+}
+
+class Info {
+  Info({
+    required this.count,
+    required this.pages,
+    required this.next,
+    this.prev,
+  });
+
+  int count;
+  int pages;
+  String next;
+  dynamic prev;
+
+  factory Info.fromJson(Map<String, dynamic> json) => Info(
+    count: json["count"],
+    pages: json["pages"],
+    next: json["next"],
+    prev: json["prev"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "count": count,
+    "pages": pages,
+    "next": next,
+    "prev": prev,
+  };
+}
+
+class Result {
+  Result({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+    required this.episode,
+    required this.url,
+    required this.created,
+  });
+
+  int id;
+  String name;
+  Status status;
+  Species species;
+  String type;
+  Gender gender;
+  Location origin;
+  Location location;
+  String image;
+  List<String> episode;
+  String url;
+  DateTime created;
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+    id: json["id"],
+    name: json["name"],
+    status: statusValues.map[json["status"]]!,
+    species: speciesValues.map[json["species"]]!,
+    type: json["type"],
+    gender: genderValues.map[json["gender"]]!,
+    origin: Location.fromJson(json["origin"]),
+    location: Location.fromJson(json["location"]),
+    image: json["image"],
+    episode: List<String>.from(json["episode"].map((x) => x)),
+    url: json["url"],
+    created: DateTime.parse(json["created"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "status": statusValues.reverse[status],
+    "species": speciesValues.reverse[species],
+    "type": type,
+    "gender": genderValues.reverse[gender],
+    "origin": origin.toJson(),
+    "location": location.toJson(),
+    "image": image,
+    "episode": List<dynamic>.from(episode.map((x) => x)),
+    "url": url,
+    "created": created.toIso8601String(),
+  };
+}
+
+enum Gender { MALE, FEMALE, UNKNOWN }
+
+final genderValues = EnumValues({
+  "Female": Gender.FEMALE,
+  "Male": Gender.MALE,
+  "unknown": Gender.UNKNOWN
+});
+
+class Location {
+  Location({
+    required this.name,
+    required this.url,
+  });
+
+  String name;
+  String url;
+
+  factory Location.fromJson(Map<String, dynamic> json) => Location(
+    name: json["name"],
+    url: json["url"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "url": url,
+  };
+}
+
+enum Species { HUMAN, ALIEN }
+
+final speciesValues = EnumValues({
+  "Alien": Species.ALIEN,
+  "Human": Species.HUMAN
+});
+
+enum Status { ALIVE, UNKNOWN, DEAD }
+
+final statusValues = EnumValues({
+  "Alive": Status.ALIVE,
+  "Dead": Status.DEAD,
+  "unknown": Status.UNKNOWN
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
